bc6c7e882fb63fd714c1a1103e55b24f1ae965f7
sagimelman <94141365+sagimelman@users.noreply.github.com>
2025-03-11 19:03:13 +0200

    Update client.py
    Without ABC!

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 from NetworkEntity import NetworkEntity
+
 # Abstract Client class
-class Client(NetworkEntity, ABC):
+class Client(NetworkEntity):
     def __init__(self, device_name: str, ip_address: str, port: int, connected: bool):
         super().__init__("Client", device_name, ip_address, port, connected)
         self.socket = None
@@ -9,13 +9,16 @@ class Client(NetworkEntity, ABC):
         self.connected = connected  # Track the connection status
 
     def connect(self):
-        raise NotImplementedError("Subclasses must implement this connect method.")
+        """Subclasses must implement this connect method."""
+        raise Exception("connect() must be implemented in a subclass.")
 
     def send_data(self, data):
-        raise NotImplementedError("Subclasses must implement this send_data method.")
+        """Subclasses must implement this send_data method."""
+        raise Exception("send_data() must be implemented in a subclass.")
 
     def receive_data(self):
-        raise NotImplementedError("Subclasses must implement this receive_data method.")
+        """Subclasses must implement this receive_data method."""
+        raise Exception("receive_data() must be implemented in a subclass.")
 
     def disconnect(self):
         if self.socket:
